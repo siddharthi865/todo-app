6ea7b6ebb39fba22ac8f1d15f63d2aa01c685f3b
siddharth865 <siddharthi865@gmail.com>
2026-03-21 20:05:24 +0530

    feat: initial commit

---
+++ src/stores/todo-store.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+
+import type { Todo } from "../types/todo";
+
+interface TodoState {
+  todos: Todo[];
+  addTodo: (title: string) => void;
+  deleteTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
+}
+
+export const useTodoStore = create<TodoState>((set) => ({
+  todos: [],
+
+  addTodo: (title) =>
+    set((state) => ({
+      todos: [
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false,
+          createdAt: Date.now(),
+        },
+        ...state.todos,
+      ],
+    })),
+
+  deleteTodo: (id) =>
+    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
+
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    })),
+}));
